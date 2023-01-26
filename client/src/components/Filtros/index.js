83c07dbb3d74c12a89a0736b9ece619c27dd7906
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-01-26 00:57:15 -0500

    backend

--- client/src/components/Filtros/index.js
+++
@@ -1,4 +0,0 @@
-export { default as FilterDiets } from './FilterDiets';
-export { default as FilterOrder } from './FilterOrder';
-export { default as FilterScore } from './FilterScore';
-export { default as Reset } from './Reset';
--- client/src/pages/index.js
+++
@@ -1,5 +0,0 @@
-export { default as About } from './About/About';
-export { default as Create } from './Create/Create';
-export { default as Details } from './Details/Details';
-export { default as Home } from './Home/Home';
-export { default as LandingPage } from './LandingPage/LandingPage'
