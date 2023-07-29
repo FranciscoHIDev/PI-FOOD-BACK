1290423f3fa3202823cf404f617028abcbf38146
Isidoro Francisco Hilario <ihilario00@gmail.com>
2023-07-28 23:20:48 -0500

    Update recipe.controller.js

--- src/controllers/recipe.controller.js
+++ src/controllers/recipe.controller.js
@@ -4,7 +4,7 @@ const axios = require('axios')
 const getApiInfo = async () => {
     try {
 
-         let dataInfo = await axios("https://run.mocky.io/v3/7e445a46-1b5f-49af-9ee5-a3d89d29b72f")
+         let dataInfo = await axios("https://run.mocky.io/v3/9c1ecdba-b59f-4cd3-a496-27082e1eb9ef")
         // let dataInfo = await axios({
         //     method: 'get',
         //     url: `https://api.spoonacular.com/recipes/complexSearch?apiKey=6032fcf8e8a04693a878e6b145158f6b&addRecipeInformation=true&number=100`,
